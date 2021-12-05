3bda8ccc019faf2a1503143f072b0fa60f8147de
cedruck <cedruck@gmail.com>
2021-12-05 20:49:36 +0100

    Day 4

---
+++ src/Day04.kt
@@ -0,0 +1,91 @@
+fun main() {
+
+    fun readBoards(input: List<String>) = input.drop(2).windowed(5, 6)
+        .map { board ->
+            board.flatMap { line ->
+                line.split(" ").filter { it != "" }
+            }.map { BingoNumber(it.toInt()) }
+        }
+
+    fun part1(input: List<String>): Int {
+        val drawnNumbers = input[0].split(',').map { it.toInt() }
+
+        val boards = readBoards(input).map { Matrix(5, 5, it) }
+            .toMutableList()
+
+        var win: Int? = null
+
+        for (draw in drawnNumbers) {
+            for ((index, board) in boards.withIndex()) {
+                val newBoard = board.findAndUpdate({ it.value == draw }, { it.pick() })
+                boards[index] = newBoard
+                if (newBoard.hasCompleteColumn() || newBoard.hasCompleteLine()) {
+                    win = draw * newBoard.all { !it.hasBeenPicked }.sumOf { it.value }
+                    break
+                }
+            }
+            if (win != null) {
+                break
+            }
+        }
+
+        return win ?: -1
+    }
+
+    data class WinableBoard(val board: Matrix<BingoNumber>, val hasWon: Boolean = false)
+
+    fun part2(input: List<String>): Int {
+        val drawnNumbers = input[0].split(',').map { it.toInt() }
+
+        val boards = readBoards(input).map { WinableBoard(Matrix(5, 5, it)) }
+            .toMutableList()
+
+        var win: Int? = null
+
+        val drawnIterator = drawnNumbers.iterator()
+        do {
+            val draw = drawnIterator.next()
+            for ((index, board) in boards.withIndex()) {
+                val newBoard = board.board.findAndUpdate({ it.value == draw }, { it.pick() })
+                boards[index] = WinableBoard(newBoard)
+                if (newBoard.hasCompleteColumn() || newBoard.hasCompleteLine()) {
+                    boards[index] = WinableBoard(newBoard, true)
+                    win = draw * newBoard.all { !it.hasBeenPicked }.sumOf { it.value }
+                }
+            }
+            boards.removeIf { it.hasWon }
+        } while (boards.size > 0)
+
+        return win ?: -1
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 4512)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
+data class BingoNumber(val value: Int, var hasBeenPicked: Boolean = false) {
+    fun pick(): BingoNumber {
+        return BingoNumber(value, true)
+    }
+}
+
+private fun Matrix<BingoNumber>.hasCompleteLine(): Boolean {
+    return this.table.windowed(this.columns, this.columns).any { lines ->
+        lines.all { it.hasBeenPicked }
+    }
+}
+
+private fun Matrix<BingoNumber>.hasCompleteColumn(): Boolean {
+    return (0 until columns)
+        .any { col ->
+            valuesForColumn(col).all {
+                it.hasBeenPicked
+            }
+        }
+}
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,37 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+data class Matrix<T>(val rows: Int, val columns: Int) {
+    internal lateinit var table: List<T>
+
+    constructor(rows: Int, columns: Int, values: List<T>) : this(rows, columns) {
+        table = values.toList()
+    }
+
+    constructor(rows: Int, columns: Int, default: T) : this(rows, columns) {
+        table = (0..columns-1 + columns*(rows-1)).map { default }.toList()
+    }
+
+    private fun updateAt(index: Int, update: (T) -> T): Matrix<T> {
+        val tempList = table.toMutableList()
+        tempList[index] = table[index].let { update.invoke(it) }
+        return Matrix<T>(rows, columns, tempList)
+    }
+
+    fun findAndUpdate(test: (T) -> Boolean, update: (T)->T): Matrix<T> {
+        val index = this.table.indexOfFirst { test.invoke(it) }
+        if (index == -1) {
+            return this
+        }
+        return updateAt(index, update)
+    }
+
+    fun all(test: (T) -> Boolean): List<T> = table.filter { test.invoke(it) }
+
+    fun valuesForColumn(col: Int): List<T> {
+        return (0 until rows).map { row ->
+            table[col + columns * row]
+        }
+    }
+}
